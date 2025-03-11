9717cf0805df5e482a5ab58362a0228f8caabb2d
Aeonoi <dzhou527@gmail.com>
2025-03-11 16:39:28 -0400

    refactor(pagerank-algorithm): rewrite the algorithm to follow a little more closely to the paper
    There are still some things that I don't think are correct or are missing from the actual algorithm.

--- link_analysis/pagerank.h
+++ link_analysis/pagerank.h
@@ -8,7 +8,7 @@
 
 class PageRank {
 public:
-  void computePageRank(Graph &graph, double damping, double tol, int maxIter);
+  void computePageRank(Graph &graph, double d, double tol, int maxIter);
 };
 
 #endif
