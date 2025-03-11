3693e3dc2b2ff2c49f21f8d0046f6ee704efaaf8
Aeonoi <dzhou527@gmail.com>
2025-03-11 16:49:27 -0400

    Revert "refactor(pagerank-algorithm): rewrite the algorithm to follow a little more closely to the paper"
    This reverts commit 9717cf0805df5e482a5ab58362a0228f8caabb2d. This
    commit will be in a different branch due to the rewrite not being fully
    done. It works and has been tested to work, it just does not fully work
    as intended (i.e. does not follow the algorithm as stated in the
    README).

--- link_analysis/main.cpp
+++ link_analysis/main.cpp
@@ -14,6 +14,11 @@ int main() {
   graph.setNode("C", 0.0);
   graph.setNode("D", 0.0);
 
+  // Suppose the graph has the following hyperlink structure:
+  // A -> B, A -> C
+  // B -> C
+  // C -> A
+  // D -> A, D -> C
   graph.setEdge("A", "B", "http://link_from_A_to_B");
   graph.setEdge("A", "C", "http://link_from_A_to_C");
   graph.setEdge("B", "C", "http://link_from_B_to_C");
@@ -26,44 +31,9 @@ int main() {
   pagerank.computePageRank(graph, 0.85, 1e-6, 100);
 
   // Display the computed PageRank scores.
-  cout << "Graph 1" << endl;
   cout << "PageRank scores:" << endl;
   unordered_map<string, double> finalNodes = graph.getNodes();
   for (const auto &entry : finalNodes) {
-    cout << "Node " << entry.first << ": " << entry.second << endl;
-  }
-
-  // Make sure that circular graphs (node that always has a link to another
-  // node) works and can end leaving an infinite loop and rank the pages/nodes
-  Graph graph1;
-
-  graph1.setNode("A", 0.0);
-  graph1.setNode("B", 0.0);
-  graph1.setNode("C", 0.0);
-  graph1.setNode("D", 0.0);
-
-  graph1.setEdge("A", "B", "http://link_from_A_to_B");
-  graph1.setEdge("A", "C", "http://link_from_A_to_C");
-  graph1.setEdge("A", "D", "http://link_from_A_to_D");
-
-  graph1.setEdge("B", "A", "http://link_from_B_to_A");
-  graph1.setEdge("B", "C", "http://link_from_B_to_C");
-  graph1.setEdge("B", "D", "http://link_from_B_to_D");
-
-  graph1.setEdge("C", "A", "http://link_from_C_to_A");
-  graph1.setEdge("C", "B", "http://link_from_C_to_B");
-  graph1.setEdge("C", "C", "http://link_from_C_to_C");
-
-  graph1.setEdge("D", "A", "http://link_from_D_to_A");
-  graph1.setEdge("D", "B", "http://link_from_D_to_B");
-  graph1.setEdge("D", "C", "http://link_from_D_to_C");
-
-  pagerank.computePageRank(graph1, 0.90, 1e-6, 1000);
-
-  cout << "Graph 2" << endl;
-  cout << "PageRank scores:" << endl;
-  unordered_map<string, double> finalNodes1 = graph1.getNodes();
-  for (const auto &entry : finalNodes1) {
     cout << "Node " << entry.first << ": " << entry.second << endl;
   }
 
